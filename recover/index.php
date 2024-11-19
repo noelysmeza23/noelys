88fc38d945ea8789f87df608279df82695c27f67
AntonnyMalu <gebrielmalu15@gmail.com>
2024-11-19 09:49:23 -0400

    primer commit

---
+++ recover/index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Recover</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="../public/NiceAdmin/assets/img/favicon.png" rel="icon">
+  <link href="../public/NiceAdmin/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+    rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="../public/NiceAdmin/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../public/NiceAdmin/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="../public/NiceAdmin/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="../public/NiceAdmin/assets/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="../public/NiceAdmin/assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="../public/NiceAdmin/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="../public/NiceAdmin/assets/vendor/simple-datatables/style.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="../public/NiceAdmin/assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: NiceAdmin
+  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
+  * Updated: Apr 20 2024 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+
+  <!--Font Awesome -->
+  <link href="../public/fontawesome/css/fontawesome.min.css" rel="stylesheet" />
+  <link href="../public/fontawesome/css/brands.css" rel="stylesheet" />
+  <link href="../public/fontawesome/css/solid.css" rel="stylesheet" />
+</head>
+
+<body>
+
+  <main>
+    <div class="container">
+
+      <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+
+              <img src="../public/img/logo.png" width="250">
+
+              <div class="card mb-3">
+
+                <div class="card-body">
+
+                  <div class="pt-4 pb-2">
+                    <h3 class="card-title text-center pb-0 fs-4">Crear Contraseña</h3>
+                    <p class="text-center small">¡Ingrese su Nueva Contraseña para restablecer su cuenta!</p>
+                  </div>
+
+                  <form class="row g-3 needs-validation" novalidate>
+
+                    <div class="col-12">
+                      <label for="yourUsername" class="form-label">Contraseña</label>
+                      <div class="input-group has-validation">
+                        <span class="input-group-text" id="inputGroupPrepend"><i class="fa-solid fa-lock"></i></i></span>
+                        <input type="password" name="username" placeholder="Ingresar Contraseña" class="form-control"
+                          id="yourUsername" required>
+                        <div class="invalid-feedback">Por favor ingrese su nombre de usuario.</div>
+                      </div>
+                    </div>
+
+                    <div class="col-12">
+                      <label for="yourUsername" class="form-label">Confirmar</label>
+                      <div class="input-group has-validation">
+                        <span class="input-group-text" id="inputGroupPrepend"><i class="fa-solid fa-lock"></i></i></span>
+                        <input type="password" name="username" placeholder="Confirmar Contraseña" class="form-control"
+                          id="yourUsername" required>
+                        <div class="invalid-feedback">Por favor ingrese su nombre de usuario.</div>
+                      </div>
+                    </div>
+
+
+
+                    <div class="col-12">
+                      <button class="btn btn-primary w-100" type="submit">Enviar</button>
+                    </div>
+                    <div class="col-12">
+                      <p class="small mb-0">Ya tienes una cuenta? <a href="../login/">Login</a></p>
+                    </div>
+                  </form>
+
+                </div>
+              </div>
+
+              <div class="credits">
+                <!-- All the links in the footer should remain intact. -->
+                <!-- You can delete the links only if you purchased the pro version. -->
+                <!-- Licensing information: https://bootstrapmade.com/license/ -->
+                <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
+
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </section>
+
+    </div>
+  </main><!-- End #main -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+      class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="../public/NiceAdmin/assets/vendor/apexcharts/apexcharts.min.js"></script>
+  <script src="../public/NiceAdmin/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../public/NiceAdmin/assets/vendor/chart.js/chart.umd.js"></script>
+  <script src="../public/NiceAdmin/assets/vendor/echarts/echarts.min.js"></script>
+  <script src="../public/NiceAdmin/assets/vendor/quill/quill.js"></script>
+  <script src="../public/NiceAdmin/assets/vendor/simple-datatables/simple-datatables.js"></script>
+  <script src="../public/NiceAdmin/assets/vendor/tinymce/tinymce.min.js"></script>
+  <script src="../public/NiceAdmin/assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="../public/NiceAdmin/assets/js/main.js"></script>
+
+
+
+</body>
+
+</html>
